c163f2b838a5b31164058c56d8a90906611a98e4
TungDao <dtung.dao@gmail.com>
2017-04-08 09:26:50 +0530

    Parse data as block
    Along with line parsing, data block is parsed as struct as following
    layout:
    + start address of block
    + end address of block
    + list of date as byte

--- parser.py
+++ parser.py
@@ -1,50 +1,55 @@
-import os, sys
+import os
+import sys
 import re
 import collections
 
+from DataLineFormat import *
+from DataBlockFormat import *
+from S19Struct import *
+
 
 class S19Parser():
-	class S19Struct():
-		header = None
-		data = dict()
-		line_cnt = None
-		start_addr = None
-		cs = None
-
-	def __init__(self, s19_file_name, isCalCk):
+	def __init__(self, s19_file_name, isCalCs):
 		self.s19_file = open(s19_file_name, "rt")
-		self.isCalCk = isCalCk
+		self.isCalCs = isCalCs
 		
-		self.s19 = self.parser(self.s19_file, self.isCalCk)
+		self.s19 = self.parser(self.s19_file, self.isCalCs)
 		
 	def getS19Header(self):
 		return self.s19.header
 	
-	def getS19Data(self):
-		return self.s19.data
+	def getS19DataLines(self):
+		return self.s19.data_lines
+		
+	def getS19DataBlocks(self):
+		return self.s19.data_blocks
 	
 	def getS19LineCount(self):
-		return self.s19.line_cnt
+		return self.s19.data_rec_cnt
 	
-	def getS19StartAddress(self):
-		return self.s19.start_addr
+	def getS19ExeStartAddress(self):
+		return self.s19.exe_start_addr
 	
-	def parser(self, s19_file, isCalCk):
-		s19 = S19Parser.S19Struct()
+	def parser(self, s19_file, isCalCs):	
+		s19 = S19Struct()
 		lines = s19_file.readlines()
 		
-		# check s-record format
-		if(line[0] != 'S'): raise Exception('Wrong S-Record file format at "%s" is not correct' % (line))
+		block_start_addr = block_end_addr = None
+		data_block = []
 		
 		for line in lines:
 			line = line.strip()
+			
+			# check s-record format
+			if(line[0] != 'S'): raise Exception('Wrong S-Record file format at "%s" is not correct' % (line))
 
+			# get the mandatory fields
 			record_type = line[0 : 2]
 			byte_cnt = int(line[2 : 4], 16)
 			cs = int(line[-2:], 16)
 			
 			# check sum verify
-			if(isCalCk):
+			if(isCalCs):
 				cs_cal = 0xFF ^ (sum([int(num, 16) for num in re.findall('..', line[2 : -2])]) & 0xFF)
 				if(cs != cs_cal): raise Exception('Check sum at line "%s" is not correct' % (line))
 			
@@ -59,35 +64,58 @@ class S19Parser():
 			elif(record_type == "S3"):
 				addr = int(line[4 : 12], 16) # 32-bit Address
 				data = line[12 : 12 + (byte_cnt * 2) - 10]
-				if(addr not in s19.data): s19.data[addr] = data
-				self.data.cs = cs
+				
+				# store data as data line format
+				s19.data_lines.append(DataLineFormat(addr, data, cs))
+				
+				# store data as data block format
+				arr_data = re.findall('..', data)
+				for byte_addr, byte in zip(range(addr, addr + len(arr_data)), arr_data):			
+					if(block_end_addr != None):
+						if((byte_addr - block_end_addr) != 1): # transit to new block
+							s19.data_blocks.append(DataBlockFormat(block_start_addr, block_end_addr, data_block))
+
+							block_start_addr = None
+							block_end_addr = None
+							data_block = []
+					block_end_addr = byte_addr
+						
+					if(block_start_addr == None): # this is the first block
+						block_start_addr = byte_addr
+					   
+					data_block.append(int(byte, 16))
 			elif(record_type == "S4"):
 				pass # This record is reserved
 			elif(record_type == "S5"):
-				if(s19.header == None): pass
+				if(s19.data_rec_cnt == None):
+					s19.data_rec_cnt = int(line[4 : 4 + (byte_cnt * 2) - 2], 16)
 				else: raise Exception('Found more than one "S5" record in file')
 			elif(record_type == "S6"):
-				if(s19.header == None): pass
-				else: raise Exception('Found more than one "S6 record in file')
+				if(s19.data_rec_cnt == None): pass
+				else: raise Exception('Found more than one "S6" record in file')
 			elif(record_type == "S7"):
-				s19.start_addr = int(line[4 : 4 + (byte_cnt * 2) - 2], 16)
+				s19.exe_start_addr = int(line[4 : 4 + (byte_cnt * 2) - 2], 16)
 			elif(record_type == "S8"):
 				raise Exception('Record field current not support')
 			elif(record_type == "S9"):
 				raise Exception('Record field current not support')
-			
+		
+		# add the last block into struct
+		s19.data_blocks.append(DataBlockFormat(block_start_addr, block_end_addr, data_block))		
+		
 		return s19
 	
 
 if __name__ == '__main__':
-	parser = S19Parser(s19_file_name=r"C:\Users\ddtkh\Desktop\s19_parser\XMC24vLow.s19", 
-					   isCalCk=False)
+	parser = S19Parser(s19_file_name=r"C:\Users\ddtkh\Desktop\s19_parser\SRec_Parser\test\sample.s19", 
+					   isCalCs=False)
 	
 	print("Header: " + parser.getS19Header())
-	print("S3 line count: %d" % parser.getS19LineCount() if parser.getS19LineCount() != None else 0)
-	print("Start address: 0x%08X" % parser.getS19StartAddress())
-	
-	#print(parser.getS19Data()[int('00FE44FF', 16)])
-	
-	for key, item in collections.OrderedDict(sorted(parser.getS19Data().items())).iteritems():
-		print(hex(key), item)
+	print("S3 line count: " + str(parser.getS19LineCount()))
+	print("Start address: 0x%08X" % parser.getS19ExeStartAddress())
+
+	for block in parser.getS19DataBlocks():
+		print("Block: ", 
+			  hex(block.block_start_addr), 
+			  hex(block.block_end_addr), 
+			  len(block.block_data))
