7535b648e111489bec9bee888b4fcd9ce2534b44
TungDao <dtung.dao@gmail.com>
2017-04-08 09:29:27 +0530

    Re-struct the project
    move from the parent folder

---
+++ parser/DataBlockFormat.py
@@ -0,0 +1,6 @@
+
+class DataBlockFormat():
+	def __init__(self, block_start_addr, block_end_addr, block_data):
+		self.block_start_addr = block_start_addr
+		self.block_end_addr = block_end_addr
+		self.block_data = block_data
---
+++ parser/S19Struct.py
@@ -0,0 +1,7 @@
+
+class S19Struct():
+	header = None
+	data_lines = [] # DataLineFormat()
+	data_blocks = [] # DataBlockFormat()
+	data_rec_cnt = None
+	exe_start_addr = None
